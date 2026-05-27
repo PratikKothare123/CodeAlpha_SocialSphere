cf6d6eec17d3ade8f0087a8978ad112433943d8b
Pratik Kothare <kotharepratik129@gmail.com>
2026-05-27 12:15:58 +0530

    Fixed Render dynamic port issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,33 +5,31 @@ import cors from 'cors';
 import { createApp } from './app.js';
 import { connectDB } from './config/db.js';
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 10000;
 
 // Connect MongoDB
 await connectDB();
 
-// Create Express App
+// Create App
 const app = createApp();
 
 // Middleware
 app.use(express.json());
 
-
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
-      "https://pratiksocialsphere-co8rkytly-pratiks-projects-7b933d8c.vercel.app",
+      'http://localhost:5173',
+      'https://pratiksocialsphere-co8rkytly-pratiks-projects-7b933d8c.vercel.app',
     ],
     credentials: true,
   })
 );
 
 // Test Route
-app.get("/", (req, res) => {
-  res.send("SocialSphere API Running 🚀");
+app.get('/', (req, res) => {
+  res.send('SocialSphere API Running 🚀');
 });
-
 
 // Start Server
 app.listen(PORT, '0.0.0.0', () => {
